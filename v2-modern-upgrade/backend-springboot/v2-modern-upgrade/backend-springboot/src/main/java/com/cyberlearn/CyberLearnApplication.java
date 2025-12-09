8b9aa676eeec8d85f8e926d4f8edbf9c8eb39985
Aditya Kumar Jaiswal <ravijaiswal822114@gmail.com>
2025-12-09 08:56:14 +0530

    CyberLearnApplication.java

---
+++ v2-modern-upgrade/backend-springboot/v2-modern-upgrade/backend-springboot/src/main/java/com/cyberlearn/CyberLearnApplication.java
@@ -0,0 +1,11 @@
+package com.cyberlearn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CyberLearnApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CyberLearnApplication.class, args);
+    }
+}
